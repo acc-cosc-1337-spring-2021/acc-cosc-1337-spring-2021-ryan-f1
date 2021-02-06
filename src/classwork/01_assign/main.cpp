f90189a794a9f580f95ca3fde60836656bc7f6ac
ryan-f1 <ryan.farley1@g.austincc.edu>
2021-02-06 12:45:26 -0600

    Homework 1 ryan farley

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,12 +1,19 @@
 //Write the include statement for types.h here
-
+#include "types.h"
 //Write include for capturing input from keyboard and displaying output to screen
-
-
+#include<iostream>
+using std::cout; using std::cin;
 
 int main() 
 {
-
+	int num, result, num1;
+	cout << "type a number to multiply it by 5: ";
+	cin >> num;
+	result = multiply_numbers(num);
+	cout << "The result is: " << result <<"\n";
+	num1 = 4;
+	result = multiply_numbers(num1);
+	cout << "and here is 4 times 5: " << result;
 	return 0;
 }
 

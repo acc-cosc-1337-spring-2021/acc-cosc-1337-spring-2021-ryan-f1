d1b4e6e066de8a5e894bc447b632b23d53177e2d
ryan-f1 <ryan.farley1@g.austincc.edu>
2021-02-09 18:47:11 -0600

    If statement examples

--- test/examples_test/03_module_test/03_module_tests.cpp
+++ test/examples_test/03_module_test/03_module_tests.cpp
@@ -1,7 +1,22 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "if.h"
+#include "if_else.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("test is even function", "returns true if num even")
+{
+	REQUIRE(is_even(4) == true);
+	REQUIRE(is_even(5) == false);
+}
+TEST_CASE("test get generation, enter yoear return generation string")
+{
+	REQUIRE(get_generation(1997) == "Centenial");
+	REQUIRE(get_generation(1990) == "Millenial");
+	REQUIRE(get_generation(1975) == "Generation X");
+	REQUIRE(get_generation(1950) == "Baby boomer");
+	REQUIRE(get_generation(1930) == "Silent Generation");
+}

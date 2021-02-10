e111299cb0595480f1b40eba492fef65db51467b
ryan-f1 <ryan.farley1@g.austincc.edu>
2021-02-09 19:09:09 -0600

    Switch case example

--- test/examples_test/03_module_test/03_module_tests.cpp
+++ test/examples_test/03_module_test/03_module_tests.cpp
@@ -2,7 +2,7 @@
 #include "catch.hpp"
 #include "if.h"
 #include "if_else.h"
-
+#include "switch.h"
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
@@ -12,11 +12,19 @@ TEST_CASE("test is even function", "returns true if num even")
 	REQUIRE(is_even(4) == true);
 	REQUIRE(is_even(5) == false);
 }
-TEST_CASE("test get generation, enter yoear return generation string")
+TEST_CASE("test get generation", "enter yoear return generation string")
 {
 	REQUIRE(get_generation(1997) == "Centenial");
 	REQUIRE(get_generation(1990) == "Millenial");
 	REQUIRE(get_generation(1975) == "Generation X");
 	REQUIRE(get_generation(1950) == "Baby boomer");
 	REQUIRE(get_generation(1930) == "Silent Generation");
+}
+TEST_CASE("test menu", "enter menu options")
+{
+	REQUIRE(menu(1) == "Option 1");
+	REQUIRE(menu(2) == "Option 2");
+	REQUIRE(menu(3) == "Option 3");
+	REQUIRE(menu(4) == "Option 4");
+	REQUIRE(menu(5) == "Invalid Option");
 }

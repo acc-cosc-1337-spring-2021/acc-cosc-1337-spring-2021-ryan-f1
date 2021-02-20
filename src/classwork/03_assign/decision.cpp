8537efd06dcab49a57b266d34fdf166fcd043466
ryan-f1 <ryan.farley1@g.austincc.edu>
2021-02-19 19:30:45 -0600

    Homework 3

--- src/classwork/03_assign/decision.cpp
+++ src/classwork/03_assign/decision.cpp
@@ -1,1 +1,59 @@
 //cpp
+#include "decision.h"
+
+
+std::string get_letter_grade_using_if(int grade)
+{
+    std::string letter_grade;
+
+    if(grade >= 90 && grade <= 100)
+    {
+        letter_grade = "A";
+    }
+     else if(grade >= 80 && grade < 90)
+    {
+        letter_grade = "B";
+    }
+     else if(grade >= 70 && grade < 80)
+    {
+        letter_grade = "C";
+    }
+     else if(grade >= 60 && grade < 70)
+    {
+        letter_grade = "D";
+    }
+    else 
+    {
+        letter_grade = "F";
+    }
+
+    return letter_grade;
+}
+
+
+std::string get_letter_grade_using_switch(int grade)
+{
+    std::string letter_grade;
+    grade = grade - (grade % 10);
+
+    switch(grade)
+    {
+        case 90: case 100:
+            letter_grade = "A";
+            break;
+        case 80:
+            letter_grade = "B";
+            break;
+        case 70:
+            letter_grade = "C";
+            break;
+        case 60:
+            letter_grade = "D";
+            break;
+        case 0: case 10: case 20: case 30: case 40: case 50:
+            letter_grade = "F";
+            break;
+    }
+    
+    return letter_grade;
+}
--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,9 +1,30 @@
 //Write the include statement for decisions.h here
-
+#include "decision.h"
 //Write namespace using statements for cout and cin
+#include<iostream>
+using std::cout; using std::cin;
 
 int main() 
 {
+
+	cout<<"Input a number grade to convert to a letter grade: ";
+	int grade;
+	std::string letter_grade;
+	cin>>grade;
+
+	if(!(grade < 0 || grade > 100))
+	{
+		letter_grade = get_letter_grade_using_if(grade);
+		cout<<"\n [Using if/else if method] Your grade is: "<< letter_grade;
+
+		letter_grade = get_letter_grade_using_switch(grade);
+		cout<<"\n [Using switch method] Your grade is: "<< letter_grade;
+
+	}
+	else 
+	{
+		cout<<"Error: Number out of range.";
+	}
 
 	return 0;
 }

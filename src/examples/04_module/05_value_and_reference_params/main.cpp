459a5359332b374cad54ba46cee91644f1302082
ryan-f1 <ryan.farley1@g.austincc.edu>
2021-03-04 13:08:25 -0600

    Example_4_reference_variables

--- src/examples/04_module/05_value_and_reference_params/main.cpp
+++ src/examples/04_module/05_value_and_reference_params/main.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
+using std::cout;
 
 int main() 
 {
+	auto num = 5;
+	cout<<"size of num in bytes: "<<sizeof(num)<<"\n";
+	cout<<"the memory address of num is: "<<&num<<"\n";
 
+	int& num_ref = num;//a clone of num
+	cout<<"Value of num_ref is: "<<num_ref;
 	return 0;
 }
--- test/examples_test/04_module_test/04_module_tests.cpp
+++ test/examples_test/04_module_test/04_module_tests.cpp
@@ -1,7 +1,23 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "value_ref.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("test call by value parameter", "a copy of value is sent to function")
+{
+	auto num1 = 5;
+	auto num2 = 0;
+	pass_by_val_and_ref(num1, num2);
+	REQUIRE(num1 == 5);
+}
+
+TEST_CASE("test call by reference parameter", "a different copy of value is sent to function")
+{
+	auto num1 = 5;
+	auto num2 = 0;
+	pass_by_val_and_ref(num1, num2);
+	REQUIRE(num2 == 50);
+}

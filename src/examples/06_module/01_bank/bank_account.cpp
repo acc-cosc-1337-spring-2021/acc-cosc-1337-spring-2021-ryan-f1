972ad02178de68374d1e9e435652233e6e556436
ryan-f1 <ryan.farley1@g.austincc.edu>
2021-03-17 16:57:10 -0500

    Example 6 bank

--- src/examples/06_module/01_bank/bank_account.cpp
+++ src/examples/06_module/01_bank/bank_account.cpp
@@ -1,1 +1,13 @@
 //bank_account.cpp
+#include "bank_account.h"
+#include<iostream>
+using std::cout;
+
+int main()
+{
+    BankAccount account;
+    cout<<sizeof(account)<<"\n";
+    auto balance = account.get_balance();
+
+    cout<<"the balance is: "<<balance;
+}

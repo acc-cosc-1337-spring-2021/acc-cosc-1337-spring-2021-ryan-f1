4a667ccc912c676ac4214a03a644b2ed0a4a4bc0
ryan-f1 <ryan.farley1@g.austincc.edu>
2021-03-19 13:52:38 -0500

    Homework 5

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -1,4 +1,5 @@
 #include "dna.h"
+
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -6,7 +7,19 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
-
+double get_gc_content(const string& dna)
+{
+    int count;
+    double gc_count = 0;
+    for(count = 0; count <= dna.length(); count++)
+    {
+        if (dna[count] == 'G' || dna[count] == 'C')
+        {
+            gc_count += 1;
+        }
+    }
+    return (gc_count / dna.length());
+}
 
 
 
@@ -14,7 +27,16 @@ Return quotient.
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
-
+string get_reverse_string(string dna)
+{
+    int count;
+    string dna_reversed;
+    for(count = dna.length() -1; count >= 0; count--)
+    {
+        dna_reversed.push_back(dna[count]);
+    }
+    return dna_reversed;
+}
 
 
 /*
@@ -28,3 +50,29 @@ c. return string
 
 */
 
+string get_dna_complement(string dna)
+{
+    int count;
+    string dna_compliment;
+
+    for(count = 0; count <= dna.length(); count++)
+    {
+        switch (get_reverse_string(dna)[count])
+        {
+        case 'A':
+            dna_compliment += 'T';
+            break;
+        
+        case 'T':
+            dna_compliment += 'A';
+            break;
+        case 'C':
+            dna_compliment += 'G';
+            break;
+        case 'G':
+            dna_compliment += 'C';
+            break;
+        }
+    }
+    return dna_compliment;
+}
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,8 @@
 //write include statements
-
+#include "dna.h"
+#include<iostream>
 //write using statements
-
+using std::cout; using std::cin; using std::string;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +13,36 @@ user enters a y or Y.
 */
 int main() 
 {
+	char again;
+	char choice;
+	string entered_sequence;
+
+	do
+	{
+		cout<<"Select mode\n";
+		cout<<"[1]: Get GC content of DNA\n";
+		cout<<"[2]: Get compliment of DNA\n";
+		cin>>choice;
+		switch (choice)
+		{
+			case '1':
+				cout<<"Enter a DNA string: ";
+				cin>>entered_sequence;
+				cout<<"The GC content of "<<entered_sequence<<"\nis: "<<get_gc_content(entered_sequence)<<"\n";
+				break;
+			
+			case '2':
+				cout<<"Enter a DNA string: ";
+				cin>>entered_sequence;
+				cout<<"The compliment of "<<entered_sequence<<"\nis: "<<get_dna_complement(entered_sequence)<<"\n";
+				break;
+		}
+
+		cout<<"Press y to quit\nPress something else to use another mode\n";
+		cin>>again;
+	} while (!(again == 'y'));
+	
+
+
 	return 0;
 }
